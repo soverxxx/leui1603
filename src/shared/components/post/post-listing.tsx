285449216ed5a41b38b492d51ea177ab821fdcce
soverxxx <30774832+soverxxx@users.noreply.github.com>
2023-02-14 20:22:00 +0300

    Update post-listing.tsx

--- src/shared/components/post/post-listing.tsx
+++ src/shared/components/post/post-listing.tsx
@@ -1111,16 +1111,18 @@ export class PostListing extends Component<PostListingProps, PostListingState> {
           <div class="row">
             <div class="col-12">
               {this.createdLine()}
-
+              {this.postTitleLine()}
+             
               {/* If it has a thumbnail, do a right aligned thumbnail */}
-              {this.mobileThumbnail()}
+              <div class="mob-thumbi">{this.mobileThumbnail()}</div>
 
               {/* Show a preview of the post body */}
               {this.showMobilePreview()}
 
+              {this.duplicatesLine()}
               {this.commentsLine(true)}
               {this.userActionsLine()}
-              {this.duplicatesLine()}
+              
               {this.removeAndBanDialogs()}
             </div>
           </div>
@@ -1131,15 +1133,15 @@ export class PostListing extends Component<PostListingProps, PostListingState> {
           <div class="row">
             {!this.props.viewOnly && this.voteBar()}
             <div class="col-sm-2 pr-0">
-              <div class="">{this.thumbnail()}</div>
+                  {this.createdLine()}
+                  {this.postTitleLine()}
+                  <div class="thumbi">{this.thumbnail()}</div>
             </div>
             <div class="col-12 col-sm-9">
               <div class="row">
-                <div className="col-12">
-                  {this.postTitleLine()}
-                  {this.createdLine()}
-                  {this.commentsLine()}
                   {this.duplicatesLine()}
+                  <div className="col-12">
+                  {this.commentsLine()}
                   {this.userActionsLine()}
                   {this.removeAndBanDialogs()}
                 </div>
